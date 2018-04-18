65c933d7fddb6c1f50df85274d7f04bee79c7164
Unknown <akbar.exp57@gmail.com>
2018-04-18 15:58:45 +0700

    init

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,31 @@
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar" style="background-color: black;">
+        <a class="navbar-brand" href="#"><strong>Navbar</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!--Collapse Content-->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <form class="form-inline">
+                <div class="md-form mt-0">
+                    <input class="form-control mr-sm-2" type="text" aria-label='Search' placeholder="Cari destinasi wisata..."
+                    name="search">
+                </div>
+                <div class="md-form mt-0">
+                    <input class="form-control mr-sm2" type="submit" name="submit" placeholder="cari">
+                </div>
+            </form>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link " href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Get Started</a>
+                </li>
+            </ul>
+        </div>
+</nav>
