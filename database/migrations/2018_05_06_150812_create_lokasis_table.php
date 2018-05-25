e5f66d3460d1a45f5e520729255ec789411e393e
FarrasR <farrasrahmatullah@gmail.com>
2018-05-25 15:26:39 +0700

    dsadadds
    adsada

--- database/migrations/2018_05_06_150812_create_lokasis_table.php
+++ database/migrations/2018_05_06_150812_create_lokasis_table.php
@@ -17,6 +17,7 @@ class CreateLokasisTable extends Migration
             $table->increments('id');
             $table->string('nama');
             $table->text('deskripsi');
+            $table->string('fotoLokasi')->nullable();
             $table->timestamps();
         });
     }
