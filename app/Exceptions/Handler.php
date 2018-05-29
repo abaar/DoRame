949c62ba6212c0e43a9c5077808bcf91d6000b5b
Unknown <akbar.exp57@gmail.com>
2018-05-29 20:22:23 +0700

    handler e gasido

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,29 +48,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        f($this->isHttpException($e))
-        {
-            switch ($e->getStatusCode()) 
-                {
-                // not found
-                case 404:
-                return redirect()->guest('/index');
-                break;
-
-                // internal error
-                case '500':
-                return redirect()->guest('/index');
-                break;
-
-                default:
-                    return $this->renderHttpException($e);
-                break;
-            }
-        }
-        else
-        {
-                return parent::render($request, $e);
-        }
         return parent::render($request, $exception);
     }
 }
