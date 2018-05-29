4a73a509ab56aef25aea65bee8ad2d001975acf5
Unknown <akbar.exp57@gmail.com>
2018-05-29 10:20:17 +0700

    dafar

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/regist/insert'
+        '/regist/insert',
+        'post/create/insert'
     ];
 }
